56d67f6ca61e6020d7d88108de79894898286cd1
Komal Parmar <komal.parmar@thoughtworks.com>
2022-11-17 18:22:10 +0530

    [Komal|Anuja|Sakshi|Saurabh|Aryan] Add. Send payload in json format on submitting

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./Form.css";
+// require('dotenv-webpack').config();
 
 function Form() {
+  const [id, setId] = useState([]);
+  const [msg, setMsg] = useState("");
+  
+  const requestBody = {
+    employees: id,
+    message: msg
+  }
+
+  function handleSubmit (e){
+    e.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'x-api-key':process.env.REACT_APP_API_KEY },
+      body: JSON.stringify(requestBody) 
+    };
+    fetch(process.env.REACT_APP_PIA_URL, requestOptions);
+  }
+  function handleID (e){
+    const employeeIdsArray = e.target.value.split(',')
+    setId(employeeIdsArray);
+  }
+  function handleMsg (e){
+    setMsg(e.target.value);
+  }
   return (
-    <div class="textCenter">
+    <div className="textCenter">
+      <form onSubmit={handleSubmit} >
       <div>
         <TextField
           id="standard-basic"
           label="Employee id"
           variant="standard"
+          required
           sx={{
             width: 300,
           }}
+          onChange={handleID}
         />
       </div>
       <div>
@@ -21,14 +49,17 @@ function Form() {
           id="standard-basic"
           label="Message"
           variant="standard"
+          required
           sx={{
             width: 300,
           }}
+          onChange={handleMsg}
         />
       </div>
-      <div class="btn">
-        <Button variant="contained">Submit</Button>
+      <div className="btn">
+        <Button variant="contained" type="submit">Submit</Button>
       </div>
+      </form>
     </div>
   );
 }
